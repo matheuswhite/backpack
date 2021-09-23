eb4d72b44aa9b3c9046a5eb5306e069312a2979d
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2021-09-23 17:30:28 -0300

    Adding an iterator struct and foreach macro #3
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

---
+++ src/include/bp_iter.h
@@ -0,0 +1,31 @@
+/**
+ * @file bp_iter.h
+ * @author Matheus T. dos Santos (tenoriomatheus0@gmail.com)
+ * @brief
+ * @version 0.1.0
+ * @date 19/09/2021
+ *
+ * @copyright Matheus T. dos Santos todos os direitos reservados (c) 2021
+ *
+ */
+#ifndef BACKPACK_ITER_H
+#define BACKPACK_ITER_H
+
+#include "bp_types.h"
+
+typedef struct bp_iter {
+    void *(*init)(struct bp_iter *self);
+    bool (*next)(struct bp_iter *self);
+    void *(*get)(struct bp_iter *self);
+    union {
+        usize *el;
+        usize idx;
+    } current;
+    void *coll;
+} bp_iter_t;
+
+#define BP_FOREACH(type, var, iter_ptr)                                      \
+    for (type *var = (iter_ptr)->init(iter_ptr); (iter_ptr)->next(iter_ptr); \
+         var       = (iter_ptr)->get(iter_ptr))
+
+#endif  // BACKPACK_ITER_H
