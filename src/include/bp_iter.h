a2c6e0f1262b6366f3e49f04208cf6e843f971e3
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2021-10-06 15:55:46 -0300

    Add documentation for iterator #7
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

--- src/include/bp_iter.h
+++ src/include/bp_iter.h
@@ -1,11 +1,14 @@
-/**
+/*!
  * @file bp_iter.h
  * @author Matheus T. dos Santos (tenoriomatheus0@gmail.com)
- * @brief
+ * @brief Specifies the generic iterator structure. The iterator is a interface with 3
+ * method. Thus, each data structure implements the way the iterator will walk
+ * through its elements. The user also que create custom iterators, for its structures,
+ * implementing the methods: init, next and get.
  * @version 0.1.0
  * @date 19/09/2021
  *
- * @copyright Matheus T. dos Santos todos os direitos reservados (c) 2021
+ * @copyright Matheus T. dos Santos all rights reserved (c) 2021
  *
  */
 #ifndef BACKPACK_ITER_H
@@ -17,20 +20,40 @@ extern "C" {
 
 #include "bp_types.h"
 
-typedef struct bp_iter {
-    void *(*init)(struct bp_iter *self);
-    bool (*next)(struct bp_iter *self);
-    void *(*get)(struct bp_iter *self);
-    union {
-        usize *el;
-        usize idx;
-    } current;
-    void *coll;
-} bp_iter_t;
-
+/*!
+ * Macro to easy iterator usage in loops.
+ * @param type The type of variable hold by the data structure represented by the
+ * iterator. The macro use this type as pointer.
+ * @param var Variable name that reference the element got by the iterator through the
+ * loops. This variable is a pointer.
+ * @param inter_ptr Reference to iterator variable.
+ */
 #define BP_FOREACH(type, var, iter_ptr)                                               \
     for (type *var = (type *) (iter_ptr)->init(iter_ptr); (iter_ptr)->next(iter_ptr); \
          var       = (type *) (iter_ptr)->get(iter_ptr))
+
+/*!
+ * Struct with iterator metadata.
+ *
+ * @note The functions inside the structures need be filled up by concrete implementation.
+ *
+ * @note The data structure represented by it's iterator is declared outside it. Only a
+ * reference is stored inside it.
+ */
+typedef struct bp_iter {
+    void *(*init)(struct bp_iter
+                      *self); /*!< Function to get the first element of data structure. */
+    bool (*next)(struct bp_iter *self); /*!< Function to move the iterator for the next
+                                           element in the sequence. Must return false if
+                                           the current element is the last. */
+    void *(*get)(
+        struct bp_iter *self); /*!< Function to get the current iterator element. */
+    union {
+        usize *el; /*!< Reference to current iterator element. */
+        usize idx; /*!< Index of current iterator element in the data structure itself. */
+    } current;     /*!< Union with the current iterator element address */
+    void *coll;    /*!< Reference to the data structure itself. */
+} bp_iter_t;
 
 #ifdef __cplusplus
 }
