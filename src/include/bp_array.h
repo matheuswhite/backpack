cfa5a7851ddfcbc3001ccf29951e02f2e5759ff7
Matheus T. dos Santos <tenoriomatheus0@gmail.com>
2021-10-06 12:26:39 -0300

    Add documentation for array header #7
    Signed-off-by: Matheus T. dos Santos <tenoriomatheus0@gmail.com>

--- src/include/bp_array.h
+++ src/include/bp_array.h
@@ -1,11 +1,12 @@
-/**
+/*!
  * @file bp_array.h
  * @author Matheus T. dos Santos (tenoriomatheus0@gmail.com)
- * @brief
+ * @brief Specifies the array structure. This structure works as a buffer, where the
+ * develop could insert the elements at the end and remove elements at any position.
  * @version 0.1.0
  * @date 19/09/2021
  *
- * @copyright Matheus T. dos Santos todos os direitos reservados (c) 2021
+ * @copyright Matheus T. dos Santos all rights reserved (c) 2021
  *
  */
 #ifndef BACKPACK_ARRAY_H
@@ -19,15 +20,15 @@ extern "C" {
 #include "bp_iter.h"
 #include "bp_types.h"
 
-typedef struct {
-    usize _element_size;
-    usize _max_size;
-    usize _size;
-    u8_t *_array;
-} bp_array_t;
-
+/*!
+ * Value representing an invalid index
+ */
 #define BP_ARRAY_INVALID_INDEX 0xffFFffFF
 
+/*!
+ * Macro to initialize a bp_array.
+ * @param array_ buffer where the elements will be stored.
+ */
 #define BP_ARRAY_INIT(array_)                                          \
     {                                                                  \
         ._element_size = sizeof((array_)[0]),                          \
@@ -35,6 +36,11 @@ typedef struct {
         ._array = (u8_t *) (array_),                                   \
     }
 
+/*!
+ * Macro to initialize a bp_array, which have initials elements.
+ * @param array_ buffer where the elements will be stored.
+ * @param size_ Number of elements already presents in the buffer.
+ */
 #define BP_ARRAY_START(array_, size_)                                        \
     {                                                                        \
         ._element_size = sizeof((array_)[0]),                                \
@@ -42,21 +48,94 @@ typedef struct {
         ._array = (u8_t *) (array_),                                         \
     }
 
+/*!
+ * Struct with metadata about an external buffer.
+ *
+ * @note This struct need an external buffer to work properly.
+ */
+typedef struct {
+    usize _element_size; /*!< Size (in bytes) of a single element in the array. */
+    usize _max_size;     /*!< Maximum number of elements in the array. */
+    usize _size;         /*!< Current number of elements in the array. */
+    u8_t *_array; /*!< Reference to the buffer, where the elements will be stored. */
+} bp_array_t;
+
+/*!
+ * Get an element from the array, based on its position.
+ * @param array Reference to bp_array.
+ * @param idx Element index.
+ * @return A reference to the desired element, or NULL if the index is out of range or the
+ * array argument is NULL.
+ */
 void *bp_array_get(bp_array_t *array, usize idx);
 
+/*!
+ * Push an element at the end of array.
+ * @param array Reference to bp_array.
+ * @param el Element to be pushed.
+ * @return 0 on success, errno otherwise.
+ */
 int bp_array_push(bp_array_t *array, void *el);
 
+/*!
+ * Delete an array element, based on its position.
+ * @param array Reference to bp_array.
+ * @param idx Element idx.
+ * @return 0 on success, errno otherwise.
+ */
 int bp_array_del(bp_array_t *array, usize idx);
 
+/*!
+ * Find the index of an element, based at some parameter related to the element. This
+ * parameter could be the element itself, or some field of its type. The match will
+ * be done based on cmp function pointer. If the cmp function pointer argument is null,
+ * then the elements will be compared with the parameter byte by byte.
+ * @param array Reference to bp_array.
+ * @param param Parameter used to compare elements.
+ * @param cmp Function to compare an element with the parameter passed at argument param.
+ * @return The index of found element, or BP_ARRAY_INVALID_INDEX if the element wasn't
+ * found or some mandatory argument is null.
+ */
 usize bp_array_find_idx(bp_array_t *array, void *param,
                         bool (*cmp)(void *el, void *param));
 
+/*!
+ * Find the element in the array, based at some parameter related to the element. This
+ * element could be the element itself, or some filed of its type. The match will be done
+ * based on cmp function pointer. If the cmp function point argument is null, then the
+ * elements will be compared with the parameter byte by byte.
+ * @param array Reference to bp_array.
+ * @param param Parameter used to compare elements.
+ * @param cmp Function to compare an element with the parameter passed at argument param.
+ * @return A reference to the found element, or NULL if the element wasn't found or some
+ * mandatory argument is null.
+ */
 void *bp_array_find(bp_array_t *array, void *param, bool (*cmp)(void *el, void *param));
 
+/*!
+ * Drop all elements in the array. After this function the array size is zero, but the
+ * element stay in the buffer.
+ * @param array Reference to bp_array.
+ * @return 0 on success, errno otherwise.
+ */
 int bp_array_clear(bp_array_t *array);
 
+/*!
+ * Get the array size.
+ * @param array Reference to bp_array.
+ * @return The size of array. Always return 0 if the argument array is null.
+ */
 usize bp_array_size(bp_array_t *array);
 
+/*!
+ * Get a iterator to walk through the bp_array.
+ *
+ * @note This function doesn't check if the array argument is null. So if this argument is
+ * null, a crash will occur. That check must be done outside the function.
+ *
+ * @param array Reference to bp_array.
+ * @return A new iterator for the bp_array.
+ */
 bp_iter_t bp_array_iter(bp_array_t *array);
 
 #ifdef __cplusplus
